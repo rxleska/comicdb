5d3722e907aa77cb74f4a24f8b64457967c341f5
Ryan Leska <45864749+rxleska@users.noreply.github.com>
2020-07-24 15:52:31 -0500

    added in table editing and deleting &barcode start

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,252 +1,311 @@
-import React from 'react';
+import React from "react";
 //import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 const apiUrl = "http://localhost:44343/api/Comic";
+var editIDs = []
 
 class Comics extends React.Component {
-  render(){
+
+  Update(e) {
+    // update entity - PUT
+    //e.preventDefault();
+    //console.log(e)
+    fetch(`${apiUrl}/${e.id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+        accept: "application/json",
+      },
+      body: JSON.stringify({
+        _id: e.id,
+        title: e.title,
+        series: e.series,
+        publisher: e.publisher,
+        issueNumber: e.issueNumber,
+      }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .then(() => {
+        window.location.reload();
+      });
+  }
+
+  Delete(e){
+    // delete entity - DELETE
+    //e.preventDefault();
+    fetch(`${apiUrl}/${e}`, {
+      method: "DELETE", //,
+      // "headers": {
+      // }
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .then(() => {
+        window.location.reload();
+      });
+  }
+  logE(e) {
+    
+  }
+  Edit(e) {
+    editIDs.push(e)
+    this.forceUpdate()
+  }
+
+  handleChange(e) {
+    this.setState({errorCopy: 'Generic error copy'});
+  }
+
+  EditDisplay(comic) {
+    if (editIDs.includes(comic.id)) {
+      return (
+        <tr>
+          {/* <td>{comic.id}</td> */}
+          <td><input title="editTitle" id="editTitle" className="form-control" 
+          type="text" value={comic.title} 
+          onChange={(e) => this.handleChange(comic.title = e.target.value)} required/></td>
+          <td><input title="editSeries" id="editSeries" className="form-control" 
+          type="text" value={comic.series}
+          onChange={(e) => this.handleChange(comic.series = e.target.value)} required/></td>
+          <td><input title="editPublishere" id="editPublisher" className="form-control" 
+          type="text" value={comic.publisher}
+          onChange={(e) => this.handleChange(comic.publisher = e.target.value)} required/></td>
+          <td><input title="editIssueNumber" id="editIssueNumber" className="form-control" 
+          type="text" value={comic.issueNumber}
+          onChange={(e) => this.handleChange(comic.issueNumber = e.target.value)} required/></td>
+          <td>
+            <button
+              className="btn btn-info"
+              type="button"
+              onClick={(e) => this.Update(comic)}
+            >
+              Save
+            </button>
+          </td>
+          <td>
+            <button
+              className="btn btn-danger"
+              type="button"
+              onClick={(e) => this.Delete(comic.id)}
+            >
+              Delete
+            </button>
+          </td>
+        </tr>)
+    } 
+    else {
+      return (
+        <tr>
+          {/* <td>{comic.id}</td> */}
+          <td>{comic.title}</td>
+          <td>{comic.series}</td>
+          <td>{comic.publisher}</td>
+          <td>{comic.issueNumber}</td>
+          <td>
+            <button
+              className="btn btn-info"
+              type="button"
+              onClick={(e) => this.Edit(comic.id)}
+            >
+              Edit
+            </button>
+          </td>
+          <td>
+            <button
+              className="btn btn-danger"
+              type="button"
+              onClick={(e) => this.Delete(comic.id)}
+            >
+              Delete
+            </button>
+          </td>
+        </tr>
+      )
+    }
+  }
+
+  render() {
     return (
       <table>
         <thead>
           <tr>
-            <th>ID</th>
+            {/* <th>ID</th> */}
             <th>Title</th>
             <th>Series</th>
             <th>Publisher</th>
             <th>IssueNumber</th>
+            <th>Edit</th>
+            <th>Del</th>
           </tr>
         </thead>
         <tbody>
-          {this.props.comics && this.props.comics.map(comic => {
-            return <tr>
-              <td>{comic.id}</td>
-              <td>{comic.title}</td>
-              <td>{comic.series}</td>
-              <td>{comic.publisher}</td>
-              <td>{comic.issueNumber}</td>
-            </tr>
-          })}
+          {this.props.comics &&
+            this.props.comics.map((comic) => {
+              return this.EditDisplay(comic);
+            })}
         </tbody>
       </table>
     );
   }
 }
 
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       comics: [],
-      id: '',
-      title: '',
-      series: '',
-      publisher: '',
-      issueNumber: ''
+      id: "",
+      title: "",
+      series: "",
+      publisher: "",
+      issueNumber: "",
     };
     this.create = this.create.bind(this);
-    this.update = this.update.bind(this);
-    this.delete = this.delete.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
-  componentDidMount(){
+  componentDidMount() {
     //get all entities - Get
     fetch(apiUrl, {
-      "method": "GET", 
-      "headers": {
+      method: "GET",
+      headers: {
         "content-type": "application/json",
-        "accept": "application/json"
-      }
+        accept: "application/json",
+      },
     })
-    .then(res => res.json())
-    .then(comicInfo => {
-      this.setState({
-        comics: comicInfo
+      .then((res) => res.json())
+      .then((comicInfo) => {
+        this.setState({
+          comics: comicInfo,
+        });
       })
-    })
-    .catch(err => { console.log(err); 
-    });
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   create(e) {
     // add entity - POST
     e.preventDefault();
     fetch(apiUrl, {
-      "method": "POST",
-      "headers": {
+      method: "POST",
+      headers: {
         "content-type": "application/json",
-        "accept": "application/json"
+        accept: "application/json",
       },
-      "body": JSON.stringify({
+      body: JSON.stringify({
         title: this.state.title,
         series: this.state.series,
         publisher: this.state.publisher,
-        issueNumber: this.state.issueNumber
+        issueNumber: this.state.issueNumber,
+      }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
       })
-    })
-    .then(response => response.json())
-    .then(response => {
-      console.log(response)
-    })
-    .catch(err => {
-      console.log(err);
-    }).then(()=>{
-      window.location.reload()
-    });
-  }
-
-  update(e) {
-    // update entity - PUT
-    e.preventDefault();
-    fetch(apiUrl, {
-      "method": "PUT",
-      "headers": {
-        "content-type": "application/json",
-        "accept": "application/json"
-      },
-      "body": JSON.stringify({
-        _id: this.state.id,
-        title: this.state.title,
-        series: this.state.series,
-        publisher: this.state.publisher,
-        issueNumber: this.state.issueNumber
+      .catch((err) => {
+        console.log(err);
       })
-    })
-    .then(response => response.json())
-    .then(response => { console.log(response);
-    })
-    .catch(err => { console.log(err); }).then(()=>{
-      window.location.reload()
-    });
-  }
-
-  delete(e) {
-    // delete entity - DELETE
-    e.preventDefault();
-    fetch(`${apiUrl}/${this.state.id}`, {
-      "method": "DELETE"//,
-      // "headers": {
-      // }
-    })
-    .then(response => response.json())
-    .then(response => {
-      console.log(response);
-    })
-    .catch(err => {
-      console.log(err);
-    }).then(()=>{
-      window.location.reload()
-    });
+      .then(() => {
+        window.location.reload();
+      });
   }
 
   handleChange(changeObject) {
-    this.setState(changeObject)
+    this.setState(changeObject);
   }
 
   render() {
     return (
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-md-8">
+      <div class="baseMargins">
+        <div class="grid-container">
+          <div class="Fom">
             <h1 className="display-4 text-center">Comic Database form</h1>
-              <form className="d-flex flex-column">
-                <legend className="text-center">Add-Update-Delete Comics</legend>
-                <label htmlFor="title">
-                  Comic Title:
-                  <input
-                    title="title"
-                    id="title"
-                    type="text"
-                    className="form-control"
-                    value={this.state.title}
-                    onChange={(e) => this.handleChange({ title: e.target.value})}
-                    required/>
-                </label>
-                <label htmlFor="series">
-                  Comic series:
-                  <input
-                    title="series"
-                    id="series"
-                    type="text"
-                    className="form-control"
-                    value={this.state.series}
-                    onChange={(e) => this.handleChange({ series: e.target.value})}
-                    required/>
-                </label>
-                <label htmlFor="publisher">
-                  Comic publisher:
-                  <input
-                    title="publisher"
-                    id="publisher"
-                    type="text"
-                    className="form-control"
-                    value={this.state.publisher}
-                    onChange={(e) => this.handleChange({ publisher: e.target.value})}
-                    required/>
-                </label>
-                <label htmlFor="issueNumber">
-                  Comic issueNumber:
-                  <input
-                    title="issueNumber"
-                    id="issueNumber"
-                    type="text"
-                    className="form-control"
-                    value={this.state.issueNumber}
-                    onChange={(e) => this.handleChange({ issueNumber: e.target.value})}
-                    required/>
-                </label>
-                <label htmlFor="id">
-                  Comic ID:
-                  <input
-                    name="id"
-                    id="id"
-                    type="text"
-                    className="form-control"
-                    value={this.state.id}
-                    onChange={(e) => this.handleChange({ id: e.target.value })}
-                    />
-                </label>
-                <button className="btn btn-primary" type='button' onClick={(e) => this.create(e)}>
-                  Add
-                </button>
-                <button className="btn btn-info" type='button' onClick={(e) => this.update(e)}>
-                    Update
-                </button>
-                <button className="btn btn-danger" type='button' onClick={(e) => this.delete(e)}>
-                    Delete
-                </button>
-              </form>
-              <Comics comics={this.state.comics}/>
+            <form className="d-flex flex-column">
+              <legend className="text-center">Add-Update-Delete Comics</legend>
+              <label htmlFor="title">
+                Comic Title:
+                <input
+                  title="title"
+                  id="title"
+                  type="text"
+                  className="form-control"
+                  value={this.state.title}
+                  onChange={(e) => this.handleChange({ title: e.target.value })}
+                  required
+                />
+              </label>
+              <label htmlFor="series">
+                Comic series:
+                <input
+                  title="series"
+                  id="series"
+                  type="text"
+                  className="form-control"
+                  value={this.state.series}
+                  onChange={(e) =>
+                    this.handleChange({ series: e.target.value })
+                  }
+                  required
+                />
+              </label>
+              <label htmlFor="publisher">
+                Comic publisher:
+                <input
+                  title="publisher"
+                  id="publisher"
+                  type="text"
+                  className="form-control"
+                  value={this.state.publisher}
+                  onChange={(e) =>
+                    this.handleChange({ publisher: e.target.value })
+                  }
+                  required
+                />
+              </label>
+              <label htmlFor="issueNumber">
+                Comic issueNumber:
+                <input
+                  title="issueNumber"
+                  id="issueNumber"
+                  type="text"
+                  className="form-control"
+                  value={this.state.issueNumber}
+                  onChange={(e) =>
+                    this.handleChange({ issueNumber: e.target.value })
+                  }
+                  required
+                />
+              </label>
+              <button
+                className="btn  btn-primary"
+                type="button"
+                onClick={(e) => this.create(e)}
+              >
+                Add
+              </button>
+            </form>
+          </div>
+          <div class="Tabl">
+            <Comics comics={this.state.comics} />
           </div>
         </div>
       </div>
-    )
+    );
   }
-
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 
 export default App;
